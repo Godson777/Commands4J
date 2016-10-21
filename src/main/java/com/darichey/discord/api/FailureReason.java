1344fbcbfd9bb7d92525027fd98d1fd07be13443
Godson <Godson777@users.noreply.github.com>
2016-10-20 22:25:19 -0400

    Added an extra failure reason.

--- src/main/java/com/darichey/discord/api/FailureReason.java
+++ src/main/java/com/darichey/discord/api/FailureReason.java
@@ -1,12 +1,20 @@
 package com.darichey.discord.api;
 
+import java.util.EnumSet;
+
 public enum FailureReason {
 
 	/**
 	 * If the author of the command-invoker is missing the Discord permissions required by
-	 * {@link com.darichey.discord.api.Command#requiredPermissions}
+	 * {@link com.darichey.discord.api.Command#userRequiredPermissions}
 	 */
 	AUTHOR_MISSING_PERMISSIONS,
+
+	/**
+	 * If the bot is missing the Discord permissions required by
+	 * {@link com.darichey.discord.api.Command#botRequiredPermissions}
+	 */
+	BOT_MISSING_DEFINED_PERMISSIONS,
 
 	/**
 	 * If the bot is missing Discord permissions (to send a message, etc.).
