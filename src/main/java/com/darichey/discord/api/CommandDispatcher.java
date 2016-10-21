75a96e516fb3d00335db9554517ebd67ac568f31
Godson <Godson777@users.noreply.github.com>
2016-10-20 22:24:13 -0400

    Added extra checks and ability for guild-specific prefixes.

--- src/main/java/com/darichey/discord/api/CommandDispatcher.java
+++ src/main/java/com/darichey/discord/api/CommandDispatcher.java
@@ -16,32 +16,43 @@ class CommandDispatcher implements IListener<MessageReceivedEvent> {
 	public void handle(MessageReceivedEvent event) {
 		String content = event.getMessage().getContent();
 		CommandRegistry registry = CommandRegistry.getForClient(event.getClient());
-		if (content.startsWith(registry.getPrefix())) {
-			String commandName = content.substring(1, content.contains(" ") ? content.indexOf(" ") : content.length());
-			Optional<Command> command = registry.getCommandByName(commandName, true);
-			if (command.isPresent()) {
-				if (command.get().isCaseSensitive() && !commandName.equals(command.get().getName()))
-					return; // If it's case sensitive, check if the cases match
+		boolean isUserDisabled = registry.isUserDisabledInGuild(event.getMessage().getGuild(), event.getMessage().getAuthor()) || registry.isUserDisabled(event.getMessage().getAuthor());
+		if (!event.getMessage().getChannel().isPrivate() && !event.getMessage().getAuthor().isBot() && !isUserDisabled) {
+			if (content.startsWith(registry.getPrefixForGuild(event.getMessage().getGuild()) == null ? registry.getPrefix() : registry.getPrefixForGuild(event.getMessage().getGuild()))) {
+				String commandName = content.substring(registry.getPrefixForGuild(event.getMessage().getGuild()) != null
+						? registry.getPrefixForGuild(event.getMessage().getGuild()).length()
+						: registry.getPrefix().length(), content.contains(" ") ? content.indexOf(" ") : content.length());
+				Optional<Command> command = registry.getCommandByName(commandName, true);
+				if (command.isPresent()) {
+					if (command.get().isCaseSensitive() && !commandName.equals(command.get().getName()))
+						return; // If it's case sensitive, check if the cases match
 
-				CommandContext context = new CommandContext(event.getMessage());
+					CommandContext context = new CommandContext(event.getMessage());
 
-				EnumSet<Permissions> requiredPermissions = command.get().getRequiredPermissions();
-				boolean hasPermission = event.getMessage().getChannel().getModifiedPermissions(event.getMessage().getAuthor()).containsAll(requiredPermissions);
-				if (hasPermission) {
-					command.get().onExecuted.accept(context);
-					if (command.get().deletesCommand()) {
-						RequestBuffer.request(() -> {
-							try {
-								event.getMessage().delete();
-							} catch (MissingPermissionsException e) {
-								command.get().onFailure.accept(context, FailureReason.BOT_MISSING_PERMISSIONS);
-							} catch (DiscordException e) {
-								e.printStackTrace();
+					EnumSet<Permissions> userRequiredPermissions = command.get().getUserRequiredPermissions();
+					EnumSet<Permissions> botRequiredPermissions = command.get().getBotRequiredPermissions();
+					boolean userHasPermission = event.getMessage().getChannel().getModifiedPermissions(event.getMessage().getAuthor()).containsAll(userRequiredPermissions);
+					boolean botHasPermission = event.getMessage().getChannel().getModifiedPermissions(event.getClient().getOurUser()).containsAll(botRequiredPermissions);
+					if (userHasPermission) {
+						if (botHasPermission) {
+							command.get().onExecuted.accept(context);
+							if (command.get().deletesCommand()) {
+								RequestBuffer.request(() -> {
+									try {
+										event.getMessage().delete();
+									} catch (MissingPermissionsException e) {
+										command.get().onFailure.accept(context, FailureReason.BOT_MISSING_PERMISSIONS);
+									} catch (DiscordException e) {
+										e.printStackTrace();
+									}
+								});
 							}
-						});
+						} else {
+							command.get().onFailure.accept(context, FailureReason.BOT_MISSING_DEFINED_PERMISSIONS);
+						}
+					} else {
+						command.get().onFailure.accept(context, FailureReason.AUTHOR_MISSING_PERMISSIONS);
 					}
-				} else {
-					command.get().onFailure.accept(context, FailureReason.AUTHOR_MISSING_PERMISSIONS);
 				}
 			}
 		}
